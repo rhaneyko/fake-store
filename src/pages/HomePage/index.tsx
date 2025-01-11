f2ed752f66624231a8a58796aaa0b2ccf2049476
rhaneyko <rhaneykohonorio@gmail.com>
2025-01-11 15:01:22 -0400

    Criação componente HomePage.

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,60 +1,21 @@
-import React, { useEffect, useState } from 'react';
-import { ApiProducts } from '../../services/api'; // Importe a API configurada
+import React from 'react';
+
 import { 
     Container,
-    Title,
-    CardsContainer,
-    CardProduct,
-    ProductTitle,
-    ProductPrice,
-    ProductDescription,
-    ProductImage
+    
  } from './styles';
+import Product from '../Product';
+import Header from '../../components/header';
 
-// Defina a interface para os dados do produto
-interface Product {
-  id: number;
-  title: string;
-  price: number;
-  description: string;
-  image: string;
-}
-
-const HomePage: React.FC = () => {
-  const [products, setProducts] = useState<Product[]>([]);
-  const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string | null>(null);
-
-  useEffect(() => {
-    const fetchProducts = async () => {
-      try {
-        const response = await ApiProducts.get<Product[]>('');
-        setProducts(response.data);
-      } catch (err: any) {
-        setError(err.message || 'Erro ao carregar os produtos');
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchProducts();
-  }, []);
+const HomePage = () => {
 
   return (
     <Container>
-      <h1>Lista de Produtos</h1>
-      <CardsContainer>
-        
-        {products.map((product) => (
-          <CardProduct key={product.id} >
-            <ProductImage src={product.image} alt={product.title}/>
-            <ProductTitle>{product.title}</ProductTitle>
-            <ProductPrice>Preço: R$ {product.price.toFixed(2)}</ProductPrice>
-          </CardProduct>
-        ))}
-      </CardsContainer>
-      <ul style={{ listStyle: 'none', padding: 0 }}>
-      </ul>
+      <Header/>
+      <h1>Home page</h1>
+
+      
+      
     </Container>
   );
 };
