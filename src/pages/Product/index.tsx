3b64392d24db7df93f4b971f36fb11c5534d7fa2
rhaneyko <rhaneykohonorio@gmail.com>
2025-01-12 17:06:25 -0400

    Estilização do card de produtos.

--- src/pages/Product/index.tsx
+++ src/pages/Product/index.tsx
@@ -1,15 +1,15 @@
 import React, { useEffect, useState } from 'react';
 
-import { 
+import {
     Container,
     CardsContainer,
     CardProduct,
     ProductTitle,
     ProductPrice,
     ProductImage
- } from './styles';
+} from './styles';
 
-import { ApiProducts } from '../../services/api';
+import { ApiGetAllProducts } from '../../services/api';
 
 import { ProductType } from '../../types/product';
 
@@ -21,7 +21,7 @@ const Product = () => {
     useEffect(() => {
         const fetchProducts = async () => {
             try {
-                const response = await ApiProducts.get<ProductType[]>('');
+                const response = await ApiGetAllProducts.get<ProductType[]>('');
                 setProducts(response.data);
             } catch (err: any) {
                 setError(err.message || 'Erro ao carregar os produtos');
@@ -36,21 +36,21 @@ const Product = () => {
     return (
         <Container>
             {loading ? (
-            <div>Carregando produtos...</div>
-        ) : error ? (
-            <div>{error}</div>
-        ) : (
-            <CardsContainer>
-        
-        {products.map((product) => (
-          <CardProduct key={product.id} >
-            <ProductImage src={product.image} alt={product.title} loading="lazy" />
-            <ProductTitle>{product.title}</ProductTitle>
-            <ProductPrice>Preço: R$ {product.price.toFixed(2)}</ProductPrice>
-          </CardProduct>
-        ))}
-      </CardsContainer>
-        )}
+                <div>Carregando produtos...</div>
+            ) : error ? (
+                <div>{error}</div>
+            ) : (
+                <CardsContainer>
+
+                    {products.map((product) => (
+                        <CardProduct key={product.id}>
+                            <ProductImage src={product.image} alt={product.title} loading="lazy" />
+                            <ProductTitle>{product.title}</ProductTitle>
+                            <ProductPrice>R$ {product.price.toFixed(2)}</ProductPrice>
+                        </CardProduct>
+                    ))}
+                </CardsContainer>
+            )}
         </Container>
     )
 }
