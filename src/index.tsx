cb65ad4f15954c5b82105213ce9d3e5aeb2c21de
rhaneyko <rhaneykohonorio@gmail.com>
2025-01-12 17:04:30 -0400

    Importação do bootstrap.

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 const root = ReactDOM.createRoot(
@@ -8,8 +9,6 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    
-      <App/>
-    
+    <App />
   </React.StrictMode>
 );
