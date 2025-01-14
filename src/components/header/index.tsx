05237f80b34a0c735403f992055eda74092815d5
rhaneyko <rhaneykohonorio@gmail.com>
2025-01-13 21:51:18 -0400

    Foi colocado um input para pesquisa de produto e adicionado o icon do carrinho.

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,18 +2,33 @@ import React from "react";
 import {
   HeaderContainer,
   Logo,
+  SearchProduct,
+  SearchIcon,
+  CartIcon,
+  SearchWrapper
 } from "./styles";
+
+
 
 const Header: React.FC = () => {
   const scrollToTop = () => {
     window.scrollTo({
-      top: 0, // Posição no topo
-      behavior: "smooth", // Animação suave
+      top: 0,
+      behavior: "smooth",
     });
   };
   return (
     <HeaderContainer>
       <Logo onClick={scrollToTop} >Fake Store</Logo>
+      <SearchWrapper>
+
+      <SearchProduct
+        placeholder="Buscar produtos"
+        
+      />
+      <SearchIcon/>
+      </SearchWrapper>
+      <CartIcon />
     </HeaderContainer>
   );
 };
--- src/components/header/styles.ts
+++ src/components/header/styles.ts
@@ -1,12 +1,16 @@
-import styled from 'styled-components';
+import styled from "styled-components";
+
+import { BsSearch, BsCart } from "react-icons/bs";
+
 
 export const HeaderContainer = styled.header`
   display: flex;
   justify-content: space-evenly;
+  align-items: center;
   width: 100%;
   padding: 10px 20px;
   background-color: #171717;
-  color: #FFF;
+  color: #fff;
   z-index: 2;
   position: fixed;
   top: 0;
@@ -17,4 +21,35 @@ export const Logo = styled.span`
   font-size: 2rem;
   font-weight: bold;
   cursor: pointer;
+`;
+
+export const SearchWrapper = styled.div`
+  position: relative;
+  width: 100%;
+  max-width: 300px;
+`;
+
+export const SearchProduct = styled.input`
+  width: 100%;
+  
+  padding: 10px;
+  border: 1px solid #171717;
+  border-radius: 4px;
+  font-size: 16px;
+  outline: none;
+  transition: border-color 0.3s;
+`;
+
+export const SearchIcon = styled(BsSearch)`
+  position: absolute;
+  right: 12px;
+  top: 50%;
+  transform: translateY(-50%);
+  color: #aaa;
+  font-size: 20px;
+  pointer-events: none;
+`;
+
+export const CartIcon = styled(BsCart)`
+  font-size: 2rem;
 `;
