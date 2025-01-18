187d311a3cb33f07ca70ed1f23b3bf4e2671fedc
rhaneyko <rhaneykohonorio@gmail.com>
2025-01-18 19:53:16 -0400

    Criação da conexão com o firebase para realizar a autenticação do usuario com Google.

---
+++ src/services/firebase/firebaseConfig.js
@@ -0,0 +1,16 @@
+// Importa o Firebase
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA0v6VrCm4jq2PVCwPt5Os9kzodEs9YrYU",
+  authDomain: "fake-store-5d479.firebaseapp.com",
+  projectId: "fake-store-5d479",
+  storageBucket: "fake-store-5d479.firebasestorage.app",
+  messagingSenderId: "573710141980",
+  appId: "1:573710141980:web:af8445b368c0d49a1aadd9"
+};
+
+// Inicializa o Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
