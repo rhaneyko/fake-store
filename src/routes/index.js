01d02b992422301175f043ef59f8fd2c8f62a42c
rhaneyko <rhaneykohonorio@gmail.com>
2025-01-18 19:52:48 -0400

    Criação de rota para a pagina de login.

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import HomePage from "../pages/HomePage";
 import ProductDetail from "../pages/ProductDetail";
 import CartPage from "../pages/CartPage";
+import LoginPage from "../pages/LoginPage";
 
 const RoutesApp = () => {
     return (
@@ -11,6 +12,7 @@ const RoutesApp = () => {
             <Route exact path="/" element={<HomePage/>}/>
             <Route path="/product/:id" element={<ProductDetail/>}/>
             <Route path="/cart" element={<CartPage/>}/>
+            <Route path="/login" element={<LoginPage/>}/>
           </Routes>
         </BrowserRouter>
     )
