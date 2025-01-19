1a94ca605d10a448315674e09c2351fc90aca20d
rhaneyko <rhaneykohonorio@gmail.com>
2025-01-19 18:44:30 -0400

    Estilização e criando as funções de autenticação com firebase para o usuario realizar login.

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -1,35 +1,97 @@
-import React from "react";
-import { useNavigate, useLocation } from "react-router-dom";
-import { auth } from "../../services/firebase/firebaseConfig";
+import React, { useState } from "react";
+import { useNavigate, useLocation, NavLink } from "react-router-dom";
+import { loginUser, loginWithGoogle } from "../../services/firebase/authService";
 import {
-  GoogleAuthProvider,
-  signInWithPopup,
-} from "firebase/auth";
-import { Container } from './styles'
+  Container,
+  FormLogin,
+  InputWrapper,
+  InputField,
+  GoogleLoginContainer,
+  ButtonGoogleLogin,
+  GoogleIcon,
+  ButttonLogin,
+  FormContainer,
+  RegisterButton,
+  RegisterButtonText,
+} from './styles'
 
 const LoginPage = () => {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [error, setError] = useState("")
   const navigate = useNavigate();
   const location = useLocation();
 
-  const redirectPath = new URLSearchParams(location.search).get("redirect") || "/";
+  const redirectPath = location.state?.from || "/";
 
-  const handleGoogleLogin = async () => {
-    const provider = new GoogleAuthProvider();
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
     try {
-      const result = await signInWithPopup(auth, provider);
-      const user = result.user;
-      console.log("Logged in user:", user);
+      await loginUser(email, password);
+      alert("Usuário logado com sucesso!");
+      setEmail("");
+      setPassword("");
 
-  
-      navigate(redirectPath);
-    } catch (error) {
-      console.error("Login Error:", error);
+      navigate(redirectPath)
+    } catch (error: any) {
+      setError(error.message);
     }
   };
+
+  const handleGoogleLogin = async () => {
+    try {
+      await loginWithGoogle();
+      alert("Usuário logado com Google!");
+
+      navigate(redirectPath)
+    } catch (error: any) {
+      setError(error.message);
+    }
+  };
+
   return (
     <Container>
-        <h1>Login</h1>
-        <button onClick={handleGoogleLogin}>Login with Google</button>
+      <h1>Login</h1>
+      <FormContainer>
+        <FormLogin onSubmit={handleLogin}>
+          <InputWrapper>
+            <InputField
+              type="email"
+              placeholder="Email"
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </InputWrapper>
+          <InputWrapper>
+            <InputField
+              type="password"
+              placeholder="Senha"
+              required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </InputWrapper>
+          <ButttonLogin type="submit">
+            <span>Fazer login</span>
+          </ButttonLogin>
+        </FormLogin>
+        <GoogleLoginContainer>
+          <span>ou</span>
+          <span>Faça login com rede social </span>
+          <ButtonGoogleLogin onClick={handleGoogleLogin} >
+            <GoogleIcon />
+          </ButtonGoogleLogin>
+        </GoogleLoginContainer>
+        <RegisterButton>
+          <NavLink to="/register" className="register-text">
+            <RegisterButtonText>
+              Quero me cadastrar
+            </RegisterButtonText>
+          </NavLink>
+        </RegisterButton>
+      </FormContainer>
+
     </Container>
   )
 }
--- src/pages/LoginPage/styles.ts
+++ src/pages/LoginPage/styles.ts
@@ -1,3 +1,95 @@
 import styled from "styled-components";
+import { FcGoogle } from "react-icons/fc";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  width: 100vw;
+  height: 100vh;
+
+  font-family: "DM Sans", sans-serif;
+`;
+
+export const FormContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 20px;
+  background-color: #D5D5D5;
+  border-radius: 40px;
+`;
+
+export const FormLogin = styled.form`
+  width: 100%;
+`;
+
+export const InputWrapper = styled.div`
+  margin: 20px;
+`;
+
+export const InputField = styled.input`
+  width: 300px;
+  height: 40px;
+  padding-left: 15px;
+  border: 1px solid #171717;
+  border-radius: 40px;
+`;
+
+export const ButttonLogin = styled.button`
+  width: 300px;
+  height: 40px;
+  margin: 20px;
+  border: 1px solid #171717;
+  border-radius: 40px;
+  background-color: #171717;
+  color: #fff;
+  font-size: 0.925rem;
+`;
+
+export const GoogleLoginContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  span {
+    font-size: 14px;
+    font-weight: bold;
+    color: #333;
+    margin: 5px;
+  }
+`;
+
+export const ButtonGoogleLogin = styled.button`
+  margin: 20px;
+  height: 50px;
+  width: 50px;
+  border: 1px solid #D5D5D5;
+  border-radius: 40px;
+`;
+
+export const GoogleIcon = styled(FcGoogle)`
+  font-size: 2rem;
+`;
+
+export const RegisterButton = styled.button`
+  width: 300px;
+  height: 40px;
+  margin: 20px;
+  border: 1px solid #171717;
+  border-radius: 40px;
+  background-color: #171717;
+
+  .register-text{
+    text-decoration: none;
+  }
+`;
+
+export const RegisterButtonText = styled.span`
+  color: #FFF;
+  text-decoration: none;
+  font-size: 0.925rem;
+`;
